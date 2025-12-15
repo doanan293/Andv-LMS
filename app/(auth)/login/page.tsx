3af1ca3139bc1be27a78215f2aacba623207d1b8
doanan293 <andoan020903@gmail.com>
2025-12-15 19:02:10 +0700

    up

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -5,6 +5,8 @@ import {
   CardDescription,
   CardHeader,
 } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import { GithubIcon } from "lucide-react";
 
 export default function LoginPage() {
@@ -14,11 +16,23 @@ export default function LoginPage() {
         Welcome back
         <CardDescription>Login with your Github Email</CardDescription>
       </CardHeader>
-      <CardContent>
+      <CardContent className="flex flex-col gap-4">
         <Button>
           <GithubIcon className="size-4" />
           Sign in with Github
         </Button>
+        <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border ">
+          <span className="relative z-10 bg-card px-2 text-muted-foreground">
+            Or continue with
+          </span>
+        </div>
+        <div className="grid gap-3">
+          <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input type="email" placeholder="m@gmail.com" />
+          </div>
+          <Button>Continue with email</Button>
+        </div>
       </CardContent>
     </Card>
   );
