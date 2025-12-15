578eff88867097fc64462f1f1ca3fd9056dd9627
Doan Van An <andv@vcsvietnam.vn>
2025-12-15 16:01:50 +0700

    up login github

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -5,9 +6,29 @@ import {
   CardDescription,
   CardHeader,
 } from "@/components/ui/card";
-import { GithubIcon } from "lucide-react";
+import { authClient } from "@/lib/auth-client";
+import { GithubIcon, Loader } from "lucide-react";
+import { useTransition } from "react";
+import { toast } from "sonner";
 
 export default function LoginPage() {
+  const [githubPending, startGithubTransition] = useTransition();
+  async function signInWithGithub() {
+    startGithubTransition(async () => {
+      await authClient.signIn.social({
+        provider: "github",
+        callbackURL: "/",
+        fetchOptions: {
+          onSuccess: () => {
+            toast.success("Signed in with Github, you will be redirected...");
+          },
+          onError: () => {
+            toast.error("Internal Server Error");
+          },
+        },
+      });
+    });
+  }
   return (
     <Card>
       <CardHeader className="text-xl">
@@ -15,9 +36,18 @@ export default function LoginPage() {
         <CardDescription>Login with your Github Email</CardDescription>
       </CardHeader>
       <CardContent>
-        <Button>
-          <GithubIcon className="size-4" />
-          Sign in with Github
+        <Button disabled={githubPending} onClick={signInWithGithub}>
+          {githubPending ? (
+            <>
+              <Loader className="size-4 animate-spin" />
+              <span>Loading...</span>
+            </>
+          ) : (
+            <>
+              <GithubIcon className="size-4" />
+              Sign in with Github
+            </>
+          )}
         </Button>
       </CardContent>
     </Card>
