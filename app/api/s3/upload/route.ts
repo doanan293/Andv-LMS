dddcb35a777aff147d9a2690c831026a27f5cbe3
doanan293 <andoan020903@gmail.com>
2025-12-21 21:18:19 +0700

    up file Object Storage

---
+++ app/api/s3/upload/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+import { z } from "zod";
+
+export const fileUploadSchema = z.object({
+  fileName: z.string().min(1, { message: "File name is required" }),
+  contentType: z.string().min(1, { message: "Content type is required" }),
+  size: z.number().min(1, { message: "Size is required" }),
+  isImage: z.boolean(),
+});
+
+export async function POST(request: Request) {
+  try {
+    const body = await request.json();
+    const validation = fileUploadSchema.safeParse(body);
+    if (!validation.success) {
+      return NextResponse.json(
+        { error: "Invalid Request Body" },
+        { status: 400 }
+      );
+    }
+  } catch {}
+}
